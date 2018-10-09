2c8ce1dd99025043ffc5e6690550ac029e90c8ad
Ægir Æxx <aegir.tomasson@gmail.com>
2018-10-09 14:43:38 +0000

    Added a models file for reference - not directly related to the project

---
+++ models.js
@@ -0,0 +1,36 @@
+const art = {
+    "images": [
+        "http://",
+        "http://"
+    ],
+    "isAuctionItem": false,
+    "title": "Nice",
+    "artistId": "HaShEdStRiNgWiThNuMbErSaNdLeTtErS",
+    "date": "2018-10-09T23:23:23.252Z",
+    "description": "Nice painting!!"
+};
+
+const artist = {
+    "name": "A",
+    "nickname": "B",
+    "address": "C",
+    "memberSince": "2018-10-09T23:23:23.252Z"
+};
+
+const customer = {
+    "name": "Customer Customersson",
+    "username": "customer1",
+    "email": "customer@ru.is",
+    "address": "Menntasveigur 1"
+};
+
+const auction = {
+    "artId": "HaShEdStRiNgWiThNuMbErSaNdLeTtErS",
+    "minimumPrice": 10,
+    "endDate": "09/09/2018 10:00:00"
+}
+
+const auctionBid = {
+    "customerId": "HaShEdStRiNgWiThNuMbErSaNdLeTtErS",
+    "price": 20
+}
