72782153eb5aa6b40131d530fc9565ff626eba2e
Ægir Æxx <aegir.tomasson@gmail.com>
2018-10-09 14:16:17 +0000

    Added schemas to the app

--- mansion-de-subastas-api/schemas/auction.js
+++ mansion-de-subastas-api/schemas/auction.js
@@ -1,3 +1,10 @@
 const Schema = require('mongoose').Schema;
 
-module.exports = new Schema();
+module.exports = new Schema({
+    artId: { type: Schema.Types.ObjectId, required: true },
+    minimumPrice: { type: Number, default: 1000 },
+    endDate: { type: Date, required: true },
+    // auctionWinner - maybe needs some logic.
+    // Should hold the ID of a valid customer that holds the highest bid.
+    auctionWinner: { type: Schema.Types.ObjectId },
+});
