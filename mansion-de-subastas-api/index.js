a581552e61003d23dbf43c92a2e2437c49c520ef
Ægir Æxx <aegir.tomasson@gmail.com>
2018-10-12 21:18:09 +0000

    Art, Artists and Customers are connected

--- mansion-de-subastas-api/index.js
+++ mansion-de-subastas-api/index.js
@@ -8,8 +8,8 @@ const port = 3000;
 const bodyParser = require('body-parser');
 const ArtistService = require('./services/artistService');
 const ArtService = require('./services/artService');
+const CustomerService = require('./services/customerService');
 // const AuctionService = require('./services/auctionService');
-// const CustomerService = require('./services/customerService');
 
 
 // Get all arts.
@@ -106,28 +106,68 @@ router.post('/artists', (req, res) => {
     artistService.createArtist(body);
 });
 
-/*
+
 // Get all the customers.
 router.get('/customers', (req, res) => {
-    return res.send(`All the customers...`);
+    const customerService = new CustomerService();
+
+    customerService.on('GET_ALL_CUSTOMERS', (data) => {
+       return res.json(data).end();
+    });
+
+    customerService.on('GET_ALL_CUSTOMERS_ERROR', (data) => {
+        return res.status(500).end();
+    });
+
+    customerService.getAllCustomers();
 });
 
 // Get a customer by id.
 router.get('/customers/:id', (req, res) => {
     const { id } = req.params;
-    return res.send(`The id: ${id} was requested!`);
+    const customerService = new CustomerService();
+
+    customerService.on('GET_CUSTOMER_BY_ID', (data) => {
+        return res.json(data).end();
+    });
+
+    customerService.on('GET_CUSTOMER_BY_ID_ERROR', (data) => {
+        return res.status(500).end();
+    });
+
+    customerService.getCustomerById(id);
 });
 
 // Create a customer.
 router.post('/customers', (req, res) => {
     const { body } = req;
-    return res.status(204).json(body);
+    const customerService = new CustomerService();
+
+    customerService.on('CREATE_CUSTOMER', (data) => {
+        return res.status(201).end();
+    });
+
+    customerService.on('CREATE_CUSTOMER_ERROR', (data) => {
+        return res.status(500).end();
+    });
+
+    customerService.createCustomer(body);
 });
 
-// Get all auction bide associated with a customer.
+// Get all auction bids associated with a customer.
 router.get('/customers/:id/auction-bids', (req, res) => {
     const { id } = req.params;
-    return res.send(`The id: ${id} was requested!`);
+    const customerService = new CustomerService();
+
+    customerService.on('GET_CUSTOMER_AUCTION_BIDS', (data) => {
+        return res.json(data).end();
+    });
+
+    customerService.on('GET_CUSTOMER_AUCTION_BIDS_ERROR', (data) => {
+        return res.status(500).end();
+    });
+
+    customerService.getCustomerAuctionBids(id);
 });
 
 // Get all auctions.
@@ -135,6 +175,7 @@ router.get('/auctions', (req, res) => {
     return res.send(`All the auctions...`);
 });
 
+/*
 // Get an auction by id.
 router.get('/auctions/:id', (req, res) => {
     const { id } = req.params;
--- mansion-de-subastas-api/services/customerService.js
+++ mansion-de-subastas-api/services/customerService.js
@@ -1,4 +1,5 @@
 const EventEmitter = require('events');
+const { Customer, connection } = require('../data/db');
 
 class CustomerService extends EventEmitter {
     constructor() {
@@ -7,27 +8,43 @@ class CustomerService extends EventEmitter {
             GET_ALL_CUSTOMERS: 'GET_ALL_CUSTOMERS',
             GET_CUSTOMER_BY_ID: 'GET_CUSTOMER_BY_ID',
             GET_CUSTOMER_AUCTION_BIDS: 'GET_CUSTOMER_AUCTION_BIDS',
-            CREATE_CUSTOMER: 'CREATE_CUSTOMER'
+            CREATE_CUSTOMER: 'CREATE_CUSTOMER',
+            GET_ALL_CUSTOMERS_ERROR: 'GET_ALL_CUSTOMERS_ERROR',
+            GET_CUSTOMER_BY_ID_ERROR: 'GET_CUSTOMER_BY_ID_ERROR',
+            GET_CUSTOMER_AUCTION_BIDS_ERROR: 'GET_CUSTOMER_AUCTION_BIDS_ERROR',
+            CREATE_CUSTOMER_ERROR: 'CREATE_CUSTOMER_ERROR'
         };
     }
     getAllCustomers() {
-        // Your implementation goes here
-        // Should emit a GET_ALL_CUSTOMERS event when the data is available
+        Customer.find({}, (err, customers) => {
+            if(err) { this.emit(this.events.GET_ALL_CUSTOMERS_ERROR, err); }
+            else { this.emit(this.events.GET_ALL_CUSTOMERS, customers); }
+        });
     };
 
     getCustomerById(id) {
-        // Your implementation goes here
-        // Should emit a GET_CUSTOMER_BY_ID event when the data is available
+        Customer.findById(id, (err, customer) => {
+            if(err) { this.emit(this.events.GET_CUSTOMER_BY_ID_ERROR, err); }
+            else { this.emit(this.events.GET_CUSTOMER_BY_ID, customer); }
+        });
     };
 
     getCustomerAuctionBids(customerId) {
-        // Your implementation goes here
-        // Should emit a GET_CUSTOMER_AUCTION_BIDS event when the data is available
+        // Needs more connections to make the query.
     };
 
-    createCustomer(customer) {
-        // Your implementation goes here
-        // Should emit a CREATE_CUSTOMER event when the data is available
+    createCustomer(body) {
+        const customer = new Customer({
+            name: body.name,
+            username: body.username,
+            email: body.email,
+            address: body.address
+        });
+
+        Customer.create(customer, (err) => {
+            if(err) { this.emit(this.events.CREATE_CUSTOMER_ERROR, err); }
+            else { this.emit(this.events.CREATE_CUSTOMER); }
+        });
     };
 };
 
